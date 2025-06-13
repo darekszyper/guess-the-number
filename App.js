4976690fe19c626b26093810e3b79e0d8ca0ca95
darekszyper <114878453+darekszyper@users.noreply.github.com>
2025-06-13 23:01:05 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StyleSheet, ImageBackground } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function App() {
+  return (
+    <LinearGradient
+      colors={["#72063c", "#ddb52f"]}
+      style={styles.rootContainer}
+    >
+      <ImageBackground
+        source={require("./assets/images/dices.png")}
+        resizeMode="cover"
+        style={styles.rootContainer}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.35,
+  },
+});
---
+++ components/PrimaryButton.js
@@ -0,0 +1,48 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton(props) {
+  function pressHandler() {}
+
+  return (
+    <View style={[styles.buttonOuterContainer, props.style && props.style]}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#410322" }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#7d0742",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
